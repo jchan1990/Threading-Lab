3b0f85d762079d1ef9a0f8236672a83ca20f8329
Jason Chan <jchan122490@gmail.com>
2016-08-03 09:08:54 -0400

    ping

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
@@ -18,98 +18,115 @@ import android.widget.Toast;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity{
-  private static final String TAG = MainActivity.class.getName();
-  private static final int PICK_IMAGE_REQUEST = 1;
-  private ImageView mImageView;
-  private Button mChooseButton;
-  private ProgressBar mProgressBar;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView mImageView;
+    private Button mChooseButton;
+    private ProgressBar mProgressBar;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    mChooseButton = (Button) findViewById(R.id.choose_button);
-    mImageView = (ImageView) findViewById(R.id.image);
-    mProgressBar = (ProgressBar)findViewById(R.id.progress);
-    mProgressBar.setMax(100);
-
-    mImageView.setImageResource(R.drawable.placeholder);
-    mChooseButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        selectImage();
-      }
-    });
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
-      Uri selectedImage = data.getData();
-      //TODO: Create the async task and execute it
-    }
-  }
-
-  // brings up the photo gallery/other resources to choose a picture
-  private void selectImage() {
-    Intent intent = new Intent();
-    intent.setType("image/*");
-    intent.setAction(Intent.ACTION_GET_CONTENT);
-    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-  }
-
-  //TODO: Fill in the parameter types
-  private class ImageProcessingAsyncTask extends AsyncTask<> {
-
-    //TODO: Fill in the parameter type
     @Override
-    protected Bitmap doInBackground() {
-      try {
-        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(params[0]));
-        return invertImageColors(bitmap);
-      } catch (FileNotFoundException e) {
-        Log.d(TAG, "Image uri is not received or recognized");
-      }
-      return null;
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    //TODO: Fill in the parameter type
-    @Override
-    protected void onProgressUpdate() {
-      super.onProgressUpdate(values);
-      //TODO: Update the progress bar
-    }
+        mChooseButton = (Button) findViewById(R.id.choose_button);
+        mImageView = (ImageView) findViewById(R.id.image);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+        mProgressBar.setMax(100);
 
-    //TODO: Fill in the parameter type
-    @Override
-    protected void onPostExecute() {
-      //TODO: Complete this method
+        mImageView.setImageResource(R.drawable.placeholder);
+        mChooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
     }
 
     @Override
-    protected void onPreExecute() {
-      super.onPreExecute();
-      //TODO: Complete this method
-    }
-
-    private Bitmap invertImageColors(Bitmap bitmap){
-      //You must use this mutable Bitmap in order to modify the pixels
-      Bitmap mutableBitmap = bitmap.copy(bitmap.getConfig(),true);
-
-      //Loop through each pixel, and invert the colors
-      for (int i = 0; i < mutableBitmap.getWidth(); i++) {
-        for(int j = 0; j < mutableBitmap.getHeight(); j++){
-          //TODO: Get the Red, Green, and Blue values for the current pixel, and reverse them
-          //TODO: Set the current pixel's color to the new, reversed value
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            //TODO: Create the async task and execute it
+            ImageProcessingAsyncTask task = new ImageProcessingAsyncTask();
+            task.execute(selectedImage);
         }
-        int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
-        //TODO: Update the progress bar. progressVal is the current progress value out of 100
-      }
-      return mutableBitmap;
     }
-  }
+
+    // brings up the photo gallery/other resources to choose a picture
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    //TODO: Fill in the parameter types
+    private class ImageProcessingAsyncTask extends AsyncTask<android.net.Uri, Integer, Bitmap> {
+
+        //TODO: Fill in the parameter type
+        @Override
+        protected Bitmap doInBackground(android.net.Uri... params) {
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(params[0]));
+                return invertImageColors(bitmap);
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "Image uri is not received or recognized");
+            }
+            return null;
+        }
+
+        //TODO: Fill in the parameter type
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            //TODO: Update the progress bar
+            mProgressBar.setProgress(values[0]);
+        }
+
+        //TODO: Fill in the parameter type
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            //TODO: Complete this method
+            mImageView.setImageBitmap(bitmap);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+            //TODO: Complete this method
+        }
+
+        private Bitmap invertImageColors(Bitmap bitmap) {
+            //You must use this mutable Bitmap in order to modify the pixels
+            Bitmap mutableBitmap = bitmap.copy(bitmap.getConfig(), true);
+
+            //Loop through each pixel, and invert the colors
+            for (int i = 0; i < mutableBitmap.getWidth(); i++) {
+                for (int j = 0; j < mutableBitmap.getHeight(); j++) {
+                    //TODO: Get the Red, Green, and Blue values for the current pixel, and reverse them
+                    int pixel = mutableBitmap.getPixel(i, j);
+
+                    int pixelBlue = Color.blue(pixel);
+                    int pixelRed = Color.red(pixel);
+                    int pixelGreen = Color.green(pixel);
+                    //TODO: Set the current pixel's color to the new, reversed value
+                    int invertBlue = 255 - pixelBlue;
+                    int invertRed = 255 - pixelRed;
+                    int invertGreen = 255 - pixelGreen;
+
+                    int invertedColor = Color.rgb(invertRed, invertGreen, invertBlue);
+                    mutableBitmap.setPixel(i, j, invertedColor);
+                }
+                int progressVal = Math.round((long) (100 * (i / (1.0 * mutableBitmap.getWidth()))));
+                //TODO: Update the progress bar. progressVal is the current progress value out of 100
+                publishProgress(progressVal);
+            }
+            return mutableBitmap;
+        }
+    }
 }
 
